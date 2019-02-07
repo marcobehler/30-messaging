5dde715ffe28d18d9f6a20897e180fc532d840b9
marco <marco@marcobehler.com>
2019-02-07 09:58:30 +0100

    Episode 2: Initial Project Setup

--- messaging-frontend/src/main/java/com/marcobehler/messagingfrontend/FileUploadController.java
+++ messaging-frontend/src/main/java/com/marcobehler/messagingfrontend/FileUploadController.java
@@ -1,35 +1,28 @@
 package com.marcobehler.messagingfrontend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
 
-    private Path fileDirectory = Paths.get("c:\\dev\\uploads\\");
-
-    private List<Path> files = new ArrayList<>();
+    @Value("${upload.directory}")
+    private Path fileDirectory;
 
     @GetMapping("/")
-    public String listUploadedFiles(Model model) {
-        model.addAttribute("files", files.stream().map(
-                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                        "serveFile", path.getFileName().toString()).build().toString())
-                .collect(Collectors.toList()));
+    public String listUploadedFiles(Model model) throws IOException {
+        model.addAttribute("files", Files.list(fileDirectory).map(p -> p.getFileName().toString()).collect(Collectors.toList()));
         return "uploadForm";
     }
 
